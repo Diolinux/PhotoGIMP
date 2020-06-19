6617d30dc05f5543488bf96dcdff47828c3c316a
Diolinux <blogdiolinux@gmail.com>
2020-06-19 16:43:30 -0300

    Add resources

---
+++ .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-resynth-fill-pattern.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+'''
+Gimp plugin "Fill with pattern seamless..."
+Front end to the resynthesizer plugin to make a seamless fill.
+
+Copyright 2011 lloyd konneker
+Idea by Rob Antonishen
+
+Version:
+1.0 lloyd konneker
+
+During development, remember to make it executable!!!
+And to remove older versions, both .scm and .py that might hide it.
+
+License:
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The GNU Public License is available at
+http://www.gnu.org/copyleft/gpl.html
+
+'''
+
+from gimpfu import *
+
+gettext.install("resynthesizer", gimp.locale_directory, unicode=True);
+
+debug = False
+
+def layer_from_pattern(image, pattern):
+  '''
+  Create a new image and layer having the same size as a pattern.
+  '''
+  new_basetype = pdb.gimp_image_base_type(image)  # same as source
+  new_layertype = pdb.gimp_drawable_type(pdb.gimp_image_get_active_layer(image))
+  pattern_width, pattern_height, bpp = pdb.gimp_pattern_get_info(pattern)
+  new_image = pdb.gimp_image_new(pattern_width, pattern_height, new_basetype)
+  # !!! Note that gimp_layer_new wants a layer type, not an image basetype
+  new_drawable = pdb.gimp_layer_new(new_image, pattern_width, pattern_height,
+    new_layertype, "Texture", 100, NORMAL_MODE)
+  pdb.gimp_image_add_layer(new_image, new_drawable, 0)
+  return new_image, new_drawable
+  
+  
+def guts(image, drawable, pattern):
+  ''' Crux of algorithm '''
+  
+  # Make drawble from pattern
+  pattern_image, pattern_layer = layer_from_pattern(image, pattern)
+  
+  # Fill it with pattern
+  # NOT pass pattern_layer.ID !!!
+  pdb.gimp_drawable_fill(pattern_layer, PATTERN_FILL)
+  
+  if debug:
+    gimp.Display(pattern_image) 
+    gimp.displays_flush() 
+  
+  # Resynthesize the selection from the pattern without using context
+  # 0,0,0: Not use_border (context), not tile horiz, not tile vert
+  # -1, -1, 0: No maps and no map weight
+  # DO pass pattern_layer.ID !!!
+  # Resynthesizer is an engine, never interactive
+  pdb.plug_in_resynthesizer(image, drawable, 0, 0, 0, pattern_layer.ID, -1, -1, 0, 0.05, 8, 300)
+      
+  # Clean up
+  if not debug:
+    # Delete image that is displayed throws RuntimeError
+    pdb.gimp_image_delete(pattern_image)
+  
+  
+def plugin_main(image, drawable, pattern):
+  '''
+  Main: the usual user-friendly precondition checking, postcondition cleanup.
+  pattern is a string
+  '''  
+  # User_friendly: if no selection, use entire image.
+  # But the resynthesizer does that for us.
+  
+  # Save/restore the context since we change the pattern
+  pdb.gimp_context_push()
+  pdb.gimp_context_set_pattern(pattern)
+  guts(image, drawable, pattern)
+  pdb.gimp_context_pop()
+  
+  gimp.displays_flush()
+
+
+register(
+  "python_fu_fill_pattern_resynth",
+  N_("Seamlessly fill with a pattern using synthesis."),
+  "Requires separate resynthesizer plugin.",
+  "Lloyd Konneker",
+  "Copyright 2011 Lloyd Konneker",
+  "2011",
+  N_("_Fill with pattern seamless..."),
+  "RGB*, GRAY*",
+  [
+    (PF_IMAGE,    "image",    "Input image", None),
+    (PF_DRAWABLE, "drawable", "Input drawable", None),
+    (PF_PATTERN,  "pattern",  _("Pattern:"), 'Maple Leaves')
+  ],
+  [],
+  plugin_main,
+  menu="<Image>/Edit",
+  domain=("resynthesizer", gimp.locale_directory)
+  )
+
+main()
---
+++ .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-resynth-sharpen.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+'''
+Gimp plugin "Sharpen by resynthesis"
+Requires resynthesizer plug_in (v2).
+
+Author:
+lloyd konneker (bootch at nc.rr.com)
+Based on smart_enlarge.scm 2000 by Paul Harrison.
+
+Version:
+1.0 lloyd konneker lkk 2010 Initial version in python.
+
+
+License:
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The GNU Public License is available at
+http://www.gnu.org/copyleft/gpl.html
+
+'''
+
+from gimpfu import *
+
+gettext.install("resynthesizer", gimp.locale_directory, unicode=True)
+
+def plugin_main(image, drawable, scale_factor):
+  '''
+  Algorithm:
+  
+  Resynthesize with:
+    corpus = smaller image
+    in map = smaller image but scaled up and down to blur
+    out map = original image
+  
+  TODO undo
+  
+  original did not accept an alpha channel
+  '''
+
+  
+  temp_image1 = pdb.gimp_image_duplicate(image)  # duplicate for in map
+  if not temp_image1:
+    raise RuntimeError, "Failed duplicate image"
+  temp_image2 = pdb.gimp_image_duplicate(image)  # duplicate for corpus
+  if not temp_image2:
+    raise RuntimeError, "Failed duplicate image"
+  temp_layer1 = pdb.gimp_image_get_active_layer(temp_image1)
+  if not temp_layer1:
+    raise RuntimeError, "Failed get active layer"
+  temp_layer2 = pdb.gimp_image_get_active_layer(temp_image2)
+  if not temp_layer2:
+    raise RuntimeError, "Failed get active layer"
+
+  
+  width = pdb.gimp_drawable_width(drawable)
+  height = pdb.gimp_drawable_height(drawable)
+  
+  
+  # scale input image down, for corpus map
+  pdb.gimp_image_scale(temp_image2, width/scale_factor, height/scale_factor)
+    
+  # scale input image way down, then back up to, to blur, for corpus
+  # Final size is same size as corpus map.
+  pdb.gimp_image_scale(temp_image1, width / (2 * scale_factor), height / (2 * scale_factor) )
+  pdb.gimp_image_scale(temp_image1, width/scale_factor, height/scale_factor)
+
+  # Resynthesize to restore details.
+  # Note there should not be a selection. TODO
+  pdb.plug_in_resynthesizer(
+      image,
+      drawable,
+      0, 0, 0,
+      temp_layer2.ID,  # corpus is smaller original
+      temp_layer1.ID,  # input map is blurred smaller original
+      drawable.ID,     # output map is original itself
+      1.0, 0.117, 8, 500) 
+
+  pdb.gimp_image_delete(temp_image1)
+  pdb.gimp_image_delete(temp_image2)
+
+
+register(
+  "python_fu_sharpen_resynthesized",
+  N_("Sharpen image by synthesis."),
+  "Requires separate resynthesizer plugin.",
+  "Lloyd Konneker",
+  "Copyright 2000 Paul Harrison, 2010 Lloyd Konneker",
+  "2010",
+  N_("Sharpen by synthesis..."),
+  "RGB*, GRAY*",
+  [ (PF_IMAGE, "image",       "Input image", None),
+    (PF_DRAWABLE, "drawable", "Input drawable", None),
+    (PF_ADJUSTMENT, "sharpen_factor", _("Sharpening:"), 2, (1, 32, 0.1)),
+  ],
+  [],
+  plugin_main,
+  menu="<Image>/Filters/Enhance",
+  domain=("resynthesizer", gimp.locale_directory)
+  )
+
+main()
+
+
