c7bc47133d480ab025efc248d9576d6856f831ce
Gabriel Almir <avlye@outlook.com>
2021-01-11 13:08:54 -0300

    Improving Plugns scripts & Add Editor Config

--- .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-heal-selection.py
+++ .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-heal-selection.py
@@ -41,46 +41,47 @@ def heal_selection(timg, tdrawable, samplingRadiusParam=50, directionParam=0, or
   if pdb.gimp_selection_is_empty(timg):
     pdb.gimp_message(_("You must first select a region to heal."))
     return
-  
+
   pdb.gimp_image_undo_group_start(timg)
-  
+
   targetBounds = tdrawable.mask_bounds
 
   # In duplicate image, create the sample (corpus).
   # (I tried to use a temporary layer but found it easier to use duplicate image.)
   tempImage = pdb.gimp_image_duplicate(timg)
+
   if not tempImage:
       raise RuntimeError, "Failed duplicate image"
-  
+
   # !!! The drawable can be a mask (grayscale channel), don't restrict to layer.
   work_drawable = pdb.gimp_image_get_active_drawable(tempImage)
   if not work_drawable:
       raise RuntimeError, "Failed get active drawable"
-      
+
   '''
   grow and punch hole, making a frisket iow stencil iow donut
-  
+
   '''
   orgSelection = pdb.gimp_selection_save(tempImage) # save for later use
   pdb.gimp_selection_grow(tempImage, samplingRadiusParam)
   # ??? returns None , docs say it returns SUCCESS
-  
+
   # !!! Note that if selection is a bordering ring already, growing expanded it inwards.
   # Which is what we want, to make a corpus inwards.
-  
+
   grownSelection = pdb.gimp_selection_save(tempImage)
-  
+
   # Cut hole where the original selection was, so we don't sample from it.
   # !!! Note that gimp enums/constants are not prefixed with GIMP_
-  pdb.gimp_image_select_item(tempImage, CHANNEL_OP_SUBTRACT, orgSelection)
-  
+  pdb.gimp_selection_combine(orgSelection, CHANNEL_OP_SUBTRACT)
+
   '''
   Selection (to be the corpus) is donut or frisket around the original target T
     xxx
     xTx
     xxx
   '''
-  
+
   # crop the temp image to size of selection to save memory and for directional healing!!
   frisketBounds = grownSelection.mask_bounds
   frisketLowerLeftX = frisketBounds[0]
@@ -91,30 +92,30 @@ def heal_selection(timg, tdrawable, samplingRadiusParam=50, directionParam=0, or
   targetLowerLeftY = targetBounds[1]
   targetUpperRightX = targetBounds[2]
   targetUpperRightY = targetBounds[3]
-  
+
   frisketWidth = frisketUpperRightX - frisketLowerLeftX
   frisketHeight = frisketUpperRightY - frisketLowerLeftY
-  
+
   # User's choice of direction affects the corpus shape, and is also passed to resynthesizer plugin
   if directionParam == 0: # all around
       # Crop to the entire frisket
-      newWidth, newHeight, newLLX, newLLY = ( frisketWidth, frisketHeight, 
+      newWidth, newHeight, newLLX, newLLY = ( frisketWidth, frisketHeight,
         frisketLowerLeftX, frisketLowerLeftY )
   elif directionParam == 1: # sides
       # Crop to target height and frisket width:  XTX
-      newWidth, newHeight, newLLX, newLLY =  ( frisketWidth, targetUpperRightY-targetLowerLeftY, 
+      newWidth, newHeight, newLLX, newLLY =  ( frisketWidth, targetUpperRightY-targetLowerLeftY,
         frisketLowerLeftX, targetLowerLeftY )
   elif directionParam == 2: # above and below
       # X Crop to target width and frisket height
       # T
       # X
-      newWidth, newHeight, newLLX, newLLY = ( targetUpperRightX-targetLowerLeftX, frisketHeight, 
+      newWidth, newHeight, newLLX, newLLY = ( targetUpperRightX-targetLowerLeftX, frisketHeight,
         targetLowerLeftX, frisketLowerLeftY )
   # Restrict crop to image size (condition of gimp_image_crop) eg when off edge of image
   newWidth = min(pdb.gimp_image_width(tempImage) - newLLX, newWidth)
   newHeight = min(pdb.gimp_image_height(tempImage) - newLLY, newHeight)
   pdb.gimp_image_crop(tempImage, newWidth, newHeight, newLLX, newLLY)
-  
+
   # Encode two script params into one resynthesizer param.
   # use border 1 means fill target in random order
   # use border 0 is for texture mapping operations, not used by this script
@@ -123,30 +124,30 @@ def heal_selection(timg, tdrawable, samplingRadiusParam=50, directionParam=0, or
   elif orderParam == 1 :  # Inward to corpus.  2,3,4
       useBorder = directionParam+2   # !!! Offset by 2 to get past the original two boolean values
   else:
-      # Outward from image center.  
+      # Outward from image center.
       # 5+0=5 outward concentric
       # 5+1=6 outward from sides
       # 5+2=7 outward above and below
       useBorder = directionParam+5
-      
+
   # Note that the old resynthesizer required an inverted selection !!
-  
+
   if debug:
     try:
-      gimp.Display(tempImage) 
+      gimp.Display(tempImage)
       gimp.displays_flush()
     except RuntimeError:  # thrown if non-interactive
       pass
     from time import sleep
     sleep(2)
-  
+
   # Not necessary to restore image to initial condition of selection, activity,
   # the original image should not have been changed,
   # and the resynthesizer should only heal, not change selection.
 
   # Note that the API hasn't changed but use_border param now has more values.
   pdb.plug_in_resynthesizer(timg, tdrawable, 0,0, useBorder, work_drawable.ID, -1, -1, 0.0, 0.117, 16, 500)
-  
+
   # Clean up (comment out to debug)
   gimp.delete(tempImage)
   pdb.gimp_image_undo_group_end(timg)
@@ -157,7 +158,7 @@ register(
   N_("Heal the selection from surroundings as if using the heal tool."),
   "Requires separate resynthesizer plugin.",
   "Lloyd Konneker",
-  "2009 Lloyd Konneker",  # Copyright 
+  "2009 Lloyd Konneker",  # Copyright
   "2009",
   N_("_Heal selection..."),
   "RGB*, GRAY*",
--- .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-resynth-sharpen.py
+++ .var/app/org.gimp.GIMP/config/GIMP/2.10/plug-ins/plugin-resynth-sharpen.py
@@ -7,6 +7,7 @@ Requires resynthesizer plug_in (v2).
 Author:
 lloyd konneker (bootch at nc.rr.com)
 Based on smart_enlarge.scm 2000 by Paul Harrison.
+Gabriel Almir, avlye
 
 Version:
 1.0 lloyd konneker lkk 2010 Initial version in python.
@@ -36,18 +37,18 @@ gettext.install("resynthesizer", gimp.locale_directory, unicode=True)
 def plugin_main(image, drawable, scale_factor):
   '''
   Algorithm:
-  
+
   Resynthesize with:
     corpus = smaller image
     in map = smaller image but scaled up and down to blur
     out map = original image
-  
+
   TODO undo
-  
+
   original did not accept an alpha channel
   '''
 
-  
+
   temp_image1 = pdb.gimp_image_duplicate(image)  # duplicate for in map
   if not temp_image1:
     raise RuntimeError, "Failed duplicate image"
@@ -61,14 +62,14 @@ def plugin_main(image, drawable, scale_factor):
   if not temp_layer2:
     raise RuntimeError, "Failed get active layer"
 
-  
+
   width = pdb.gimp_drawable_width(drawable)
   height = pdb.gimp_drawable_height(drawable)
-  
-  
+
+
   # scale input image down, for corpus map
   pdb.gimp_image_scale(temp_image2, width/scale_factor, height/scale_factor)
-    
+
   # scale input image way down, then back up to, to blur, for corpus
   # Final size is same size as corpus map.
   pdb.gimp_image_scale(temp_image1, width / (2 * scale_factor), height / (2 * scale_factor) )
@@ -83,7 +84,7 @@ def plugin_main(image, drawable, scale_factor):
       temp_layer2.ID,  # corpus is smaller original
       temp_layer1.ID,  # input map is blurred smaller original
       drawable.ID,     # output map is original itself
-      1.0, 0.117, 8, 500) 
+      1.0, 0.117, 8, 500)
 
   pdb.gimp_image_delete(temp_image1)
   pdb.gimp_image_delete(temp_image2)
